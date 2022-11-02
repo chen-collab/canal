13b03d74209aa44e9fe1602457b34b2c43a24149
chen <collab>
2022-11-02 13:07:08 +0800

    优化适配器文件监听 监听rdb目录下及其子目录下所有yml文件

--- client-adapter/rdb/src/main/java/com/alibaba/otter/canal/client/adapter/rdb/monitor/RdbConfigMonitor.java
+++ client-adapter/rdb/src/main/java/com/alibaba/otter/canal/client/adapter/rdb/monitor/RdbConfigMonitor.java
@@ -6,8 +6,14 @@ import com.alibaba.otter.canal.client.adapter.support.MappingConfigsLoader;
 import com.alibaba.otter.canal.client.adapter.support.Util;
 import com.alibaba.otter.canal.client.adapter.support.YamlUtils;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
+
 import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.io.filefilter.HiddenFileFilter;
 import org.apache.commons.io.monitor.FileAlterationListenerAdaptor;
 import org.apache.commons.io.monitor.FileAlterationMonitor;
 import org.apache.commons.io.monitor.FileAlterationObserver;
@@ -34,13 +40,15 @@ public class RdbConfigMonitor {
         this.envProperties = envProperties;
         File confDir = Util.getConfDirPath(adapterName);
         try {
+            //监听rdb 目录中的目录以及以yml为后缀的文件
+            //https://blog.csdn.net/skyupward/article/details/104837466
             FileAlterationObserver observer = new FileAlterationObserver(confDir,
-                FileFilterUtils.and(FileFilterUtils.fileFileFilter(), FileFilterUtils.suffixFileFilter("yml")));
+                FileFilterUtils.or(FileFilterUtils.and(FileFilterUtils.directoryFileFilter(), HiddenFileFilter.VISIBLE),
+                        FileFilterUtils.and(FileFilterUtils.fileFileFilter(), FileFilterUtils.suffixFileFilter("yml"))));
             FileListener listener = new FileListener();
             observer.addListener(listener);
             fileMonitor = new FileAlterationMonitor(3000, observer);
             fileMonitor.start();
-
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
@@ -60,8 +68,19 @@ public class RdbConfigMonitor {
         public void onFileCreate(File file) {
             super.onFileCreate(file);
             try {
+                if(file.isDirectory()){
+                    return;
+                }
                 // 加载新增的配置文件
-                String configContent = MappingConfigsLoader.loadConfig(adapterName + File.separator + file.getName());
+                String dir=adapterName + File.separator;
+                logger.info("加载新增的配置文件: [{}] [{}] [{}] to canal adapter", adapterName , File.separator , file.getParent());
+                String configContent =null;
+                if(file.getParent().endsWith(adapterName)){
+                    configContent =  MappingConfigsLoader.loadConfig(adapterName + File.separator + file.getName());
+                }else {
+                    String fileName=file.getAbsolutePath().substring(file.getParent().lastIndexOf(File.separator)+1);
+                    configContent = MappingConfigsLoader.loadConfig(adapterName + File.separator + fileName);
+                }
                 MappingConfig config = YamlUtils
                     .ymlToObj(null, configContent, MappingConfig.class, null, envProperties);
                 if (config == null) {
@@ -82,10 +101,21 @@ public class RdbConfigMonitor {
             super.onFileChange(file);
 
             try {
+                if(file.isDirectory()){
+                    return;
+                }
                 if (rdbAdapter.getRdbMapping().containsKey(file.getName())) {
                     // 加载配置文件
-                    String configContent = MappingConfigsLoader
-                        .loadConfig(adapterName + File.separator + file.getName());
+                    logger.info("加载配置文件: [{}] [{}] [{}] to canal adapter", adapterName , File.separator , file.getPath());
+                    String configContent =null;
+                    if(file.getParent().endsWith(adapterName)){
+                        configContent = MappingConfigsLoader
+                                .loadConfig(adapterName + File.separator + file.getName());
+                    }else {
+                        String fileName=file.getAbsolutePath().substring(file.getParent().lastIndexOf(File.separator)+1);
+                        configContent = MappingConfigsLoader.loadConfig(adapterName + File.separator + fileName);
+                    }
+
                     if (configContent == null) {
                         onFileDelete(file);
                         return;
@@ -109,11 +139,23 @@ public class RdbConfigMonitor {
             super.onFileDelete(file);
 
             try {
-                if (rdbAdapter.getRdbMapping().containsKey(file.getName())) {
-                    rdbAdapter.deleteConfig(file.getName());
+                //删除目录则目录下的配置文件一并删除
+                if(file.isDirectory()){
+                    List<File> fileList = Arrays.stream(file.listFiles()).collect(Collectors.toList());
+                    for (File file1 : fileList) {
+                        if (rdbAdapter.getRdbMapping().containsKey(file1.getName())) {
+                            rdbAdapter.deleteConfig(file1.getName());
+                            logger.info("Delete a rdb mapping config: {} of canal adapter", file1.getName());
+                        }
+                    }
+                }else {
+                    if (rdbAdapter.getRdbMapping().containsKey(file.getName())) {
+                        rdbAdapter.deleteConfig(file.getName());
 
-                    logger.info("Delete a rdb mapping config: {} of canal adapter", file.getName());
+                        logger.info("Delete a rdb mapping config: {} of canal adapter", file.getName());
+                    }
                 }
+
             } catch (Exception e) {
                 logger.error(e.getMessage(), e);
             }
